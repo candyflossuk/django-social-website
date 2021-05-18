992139cb8d0852830202bde2b0370e05593a32fe
candyflossuk <ross.a.humphrey@googlemail.com>
2021-05-18 08:41:08 +0100

    Added a URL cleaner to the ImageCreateForm that only allows for JPEG and JPG as file extensions/image types

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -8,3 +8,17 @@ class ImageCreateForm(forms.ModelForm):
         fields = ("title", "url", "description")
         # HiddenInput used to hide this field - as will be sent from JS tool
         widgets = {"url": forms.HiddenInput}
+
+    def clean_url(self):
+        """
+        - Get value of the url field by accessing the cleaned_data dict of the form instance
+        - Split the URL to get the file extension and check whether it is one of the valid extensions.
+        """
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg"]
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError(
+                "The given URL does not match valid image extensions"
+            )
+        return url
